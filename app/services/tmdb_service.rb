98535f7483ecb09ce2b77821a283f5afeb526d0d
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-05-01 16:17:06 -0400

    set and fixed getting seasons in the tmdb service

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -25,13 +25,26 @@ class TmdbService
         end
     end
 
+    #quick note, so the idea here is once you pull the show you then have the season number. I'll then create buttons that call the season list and THEN you can add that to your Seasons. You can then review the show or the season. THAT's IT
     def get_show_by_id(query)
         response = Faraday.get("https://api.themoviedb.org/3/tv/#{query}?api_key=#{ENV['TMDB_KEY']}")
         if response.status == 200
-            result = JSON.parse(response.body)['results']
+            result = JSON.parse(response.body)
             return result
             #return response
         else
+            return nil
+        end
+    end
+
+    def get_season_by_id(id)
+        response = Faraday.get("https://api.themoviedb.org/3/tv/#{id}/seasons?api_key=#{ENV['TMDB_KEY']}")
+        if response.status == 200
+            result = JSON.parse(response.body)
+            return result
+            #return response
+        else
+            #eturn response
             return nil
         end
     end
