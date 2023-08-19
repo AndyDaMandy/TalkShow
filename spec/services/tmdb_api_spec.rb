0f595298d7162030a3fb609713fd7618f37994de
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-08-18 20:53:48 -0400

    finished adding service tests for relevant tests

--- spec/services/tmdb_api_spec.rb
+++ spec/services/tmdb_api_spec.rb
@@ -32,6 +32,41 @@ RSpec.describe 'TMDB API Testing' do
       expect(show['id']).to eq(66573)
     end
 
+    it 'gets the season id via get_season_by_id' do
+      hash = TmdbService.new
+      season = hash.get_season_by_id(66573, 2)
+      puts season
+      expect(season).to_not eql(nil)
+    end
+
+    it 'gets the credits of a show when an ID is provided' do
+      hash = TmdbService.new
+      credits = hash.get_credits_by_id(66573)
+      puts credits
+      expect(credits).to_not eql(nil)
+    end
+
+    it 'gets shows, tv, movies, and people based on a query' do
+      hash = TmdbService.new
+      multi_search = hash.multi_search('The Good Place')
+      puts multi_search
+      expect(multi_search).to_not eql(nil)
+    end
+
+    it 'gets person details' do
+      hash = TmdbService.new
+      person = hash.person_details(1)
+      puts person
+      expect(person).to_not eql(nil)
+    end
+
+    it 'gets person credits (stuff they worked on)' do
+      hash = TmdbService.new
+      person = hash.person_credits(1)
+      puts person
+      expect(person).to_not eql(nil)
+    end
+
   end
 
 
